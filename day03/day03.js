dda646a097e547c8fbb1e6b3250e348f64051856
arzulo <zanemrau@gmail.com>
2024-12-12 02:29:02 -0800

    Day 3 part 1 solution

--- day03/day03.js
+++ day03/day03.js
@@ -1,3 +1,16 @@
 const InputParser = require('../src/InputParser');
 
 parser = new InputParser();
+
+let input = parser.input;
+
+let regex = /mul\((\d+),(\d+)\)/g;
+let multiplies = [...input.matchAll(regex)];
+
+
+let res = 0;
+multiplies.forEach( e => {
+    res += Number(e[1]) * Number(e[2]);
+})
+
+console.log(`Part 1 answer: ${res}`);
