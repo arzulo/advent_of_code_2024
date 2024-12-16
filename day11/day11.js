35ed071341fc4aab431f66693fc2d2deb7d3888a
arzulo <zanemrau@gmail.com>
2024-12-16 14:15:07 -0800

    Day 11 part 1

--- day11/day11.js
+++ day11/day11.js
@@ -3,12 +3,12 @@ const InputParser = require('../src/InputParser');
 let parser = new InputParser();
 let stones = parser.splitLines().parseNumArr().input[0];
 
-
 // Vars to hold part1/2 answers
 let part1 = 0;
 let part2 = 0;
 
-const BLINKS = 25;
+const BLINKSP1 = 25 ;
+const BLINKSP2 = 75;
 
 
 let blink = function(_stones) {
@@ -20,7 +20,7 @@ let blink = function(_stones) {
             newStones.push(1);
         } else if( !(String(stone).length % 2)) { // even length
             let _strStone = String(stone);
-            let _len = stone.length;
+            let _len = _strStone.length;
             let _newHalf1 = _strStone.slice(0, _len/2);
             let _newHalf2 = _strStone.slice(_len/2,_len);
             newStones.push(Number(_newHalf1), Number(_newHalf2));
@@ -32,10 +32,20 @@ let blink = function(_stones) {
 
 }
 
-for(var i = 0; i < BLINKS; i++) {
-    stones = blink(stones);
+let printStones = function() {
+    // console.log(`Current stones: ${stones}`);
 }
-part1 = stones.length;
+
+// Gonna have to offload to disk and filestream, or chunk... too big of an array :'(
+printStones();
+for(var i = 0; i < BLINKSP2; i++) {
+    if(i === BLINKSP1) {
+        part1 = stones.length;
+    }
+    stones = blink(stones);
+    printStones();
+}
+part2 = stones.length;
 
 
 // Answers
